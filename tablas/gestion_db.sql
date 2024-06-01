06f6461b4b1bec32ef22b4681be502f5591780e7
ivanros02 <ivanrosendo1102@gmail.com>
2024-06-01 17:04:58 -0300

    db

--- tablas/gestion_db.sql
+++ tablas/gestion_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-05-2024 a las 22:49:32
+-- Tiempo de generación: 01-06-2024 a las 22:04:23
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `gestion_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividades`
+--
+
+CREATE TABLE `actividades` (
+  `descripcion` varchar(255) NOT NULL,
+  `id` int(255) NOT NULL,
+  `codigo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `actividades`
+--
+
+INSERT INTO `actividades` (`descripcion`, `id`, `codigo`) VALUES
+('musicoterapia', 4, '509001');
 
 -- --------------------------------------------------------
 
@@ -51,9 +70,35 @@ INSERT INTO `disponibilidad` (`id`, `id_prof`, `dia_semana`, `hora_inicio`, `hor
 
 CREATE TABLE `especialidad` (
   `id_especialidad` int(11) NOT NULL,
-  `cod_especialidad` int(255) NOT NULL,
   `desc_especialidad` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `especialidad`
+--
+
+INSERT INTO `especialidad` (`id_especialidad`, `desc_especialidad`) VALUES
+(22, 'musicoterapia'),
+(25, 'gimnasia');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `obra_social`
+--
+
+CREATE TABLE `obra_social` (
+  `id` int(11) NOT NULL,
+  `siglas` varchar(255) NOT NULL,
+  `razon_social` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `obra_social`
+--
+
+INSERT INTO `obra_social` (`id`, `siglas`, `razon_social`) VALUES
+(4, 'INSSJP', 'PAMI');
 
 -- --------------------------------------------------------
 
@@ -97,6 +142,35 @@ CREATE TABLE `profesional` (
   `telefono` int(255) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `profesional`
+--
+
+INSERT INTO `profesional` (`id_prof`, `nombreYapellido`, `id_especialidad`, `domicilio`, `localidad`, `codigo_pos`, `matricula_p`, `matricula_n`, `telefono`, `email`) VALUES
+(11, 'PEREZ MURGA CARLOS ISMAEL', 22, '143', 'Berazategui', '1886', '123', '345', 1139114579, 'ivanrosendo1102@gmail.com'),
+(12, 'WALTER ROSENDO', 25, '143', 'Villa Espana', '1886', '218', '796', 243567189, 'infowss@gmail.com'),
+(14, 'messi', 25, '2651', 'Berazategui', '1886', '111', '222', 2147483647, 'ivanrosendo1102@gmail.com'),
+(15, 'LIVINGSTON ESTELA BEATRIZ', 22, '2651', 'Villa Espana', '1886', '123', '222', 2147483647, 'TEST34@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_egreso`
+--
+
+CREATE TABLE `tipo_egreso` (
+  `id` int(11) NOT NULL,
+  `codigo` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_egreso`
+--
+
+INSERT INTO `tipo_egreso` (`id`, `codigo`, `descripcion`) VALUES
+(1, 'AT', 'ABANDONO DE TRATAMIENTO');
 
 -- --------------------------------------------------------
 
@@ -169,6 +243,12 @@ INSERT INTO `usuarios` (`id`, `usuario`, `clave`) VALUES
 --
 
 --
+-- Indices de la tabla `actividades`
+--
+ALTER TABLE `actividades`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `disponibilidad`
 --
 ALTER TABLE `disponibilidad`
@@ -182,6 +262,12 @@ ALTER TABLE `especialidad`
   ADD PRIMARY KEY (`id_especialidad`);
 
 --
+-- Indices de la tabla `obra_social`
+--
+ALTER TABLE `obra_social`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `paciente`
 --
 ALTER TABLE `paciente`
@@ -193,6 +279,12 @@ ALTER TABLE `paciente`
 ALTER TABLE `profesional`
   ADD PRIMARY KEY (`id_prof`),
   ADD KEY `especialidad` (`id_especialidad`);
+
+--
+-- Indices de la tabla `tipo_egreso`
+--
+ALTER TABLE `tipo_egreso`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `turnos`
@@ -218,6 +310,12 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `actividades`
+--
+ALTER TABLE `actividades`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de la tabla `disponibilidad`
 --
 ALTER TABLE `disponibilidad`
@@ -227,7 +325,13 @@ ALTER TABLE `disponibilidad`
 -- AUTO_INCREMENT de la tabla `especialidad`
 --
 ALTER TABLE `especialidad`
-  MODIFY `id_especialidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_especialidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `obra_social`
+--
+ALTER TABLE `obra_social`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `paciente`
@@ -239,7 +343,13 @@ ALTER TABLE `paciente`
 -- AUTO_INCREMENT de la tabla `profesional`
 --
 ALTER TABLE `profesional`
-  MODIFY `id_prof` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_prof` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_egreso`
+--
+ALTER TABLE `tipo_egreso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `turnos`
@@ -267,7 +377,7 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `profesional`
 --
 ALTER TABLE `profesional`
-  ADD CONSTRAINT `profesional_ibfk_1` FOREIGN KEY (`id_especialidad`) REFERENCES `especialidad` (`id_especialidad`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_profesional_especialidad` FOREIGN KEY (`id_especialidad`) REFERENCES `especialidad` (`id_especialidad`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
